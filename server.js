fcb3f21be87ab24e83b00047a6d20d65dddc912d
Pandora Becker Lovatto <Pandora-blovatto@educar.rs.gov.br>
2023-08-30 17:56:48 -0300

    finalização 3.0

--- server.js
+++ server.js
@@ -56,5 +56,5 @@ app.use("/redefinir", redefinirRoutes);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta http://localhost:${PORT}`);
 });
